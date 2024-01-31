63ad9bdbd1dfd9ab816dcf7d139e8a173d18f83e
hari <hari123@email.com>
2024-01-31 11:12:27 +0000

    solution

---
+++ src/components/Home/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+import Message from '../Message'
+import Login from '../Login'
+import Logout from '../Logout'
+
+import {Component} from 'react'
+
+class Home extends Component {
+  state = {
+    isClicked: false,
+  }
+  changeToText = () => {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked,
+    }))
+  }
+  render() {
+    const {isClicked} = this.state
+    if (isClicked) {
+      return <Logout />
+    }
+    return (
+      <div className="bg-container">
+        <div className="container">
+          <Message />
+          <Login onClick={this.changeToText} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
